4d467ce721e4f83f7d49c50f0074695ebe5840c6
amaanmajeed <amaanmajid123@gmail.com>
2022-02-20 00:08:33 +0500

    Updated Exception Error

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pyttsx3
+import speech_recognition as sr
+
+#Install Pyaudio : https://www.youtube.com/watch?v=mlrxIQRK3SE
+
 
 Assistant = pyttsx3.init('sapi5')
 voices = Assistant.getProperty('voices')
@@ -6,11 +10,33 @@ print(voices)
 Assistant.setProperty('voices', voices[0].id)
 
 
-def Talk(audio):
+def speak(audio):
     print("     ")
     Assistant.say(audio)
     print("     ")
     Assistant.runAndWait()
 
 
-Talk("Hello, Sir")
+def takecommad():
+    command = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening..")
+        command.pause_threshold = 1
+        audio = command.listen(source)
+
+        try:
+            print("Recognising")
+            quary = command.recognize_google(audio)
+            print(f"You said : {quary}")
+        except Exception as Error:
+            return "none"
+
+        return quary.lower()
+
+
+quary = takecommad()
+
+if 'Hello' in quary:
+    speak("Hello Sir")
+else:
+    quary = takecommad()
